37799f72b4b9a2e22bffc88b89f91e71cd9e50ec
Kevando <Kevandogordon@yahoo.com>
2022-02-12 11:06:24 -0500

    Added Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\FileController;
-use App\Http\Controllers\CarDetailController;
-use App\Http\Controllers\Auth\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +14,16 @@ use App\Http\Controllers\Auth\RegisterController;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+
 Route::get('index', function () {
     return view('index');
 });
@@ -31,47 +36,6 @@ Route::get('contact', function () {
     return view('contact');
 });
 
-// Route::get('adminlogin', function () {
-//     return view('admin/index');
-// });
-
-Route::get('dashboard', function () {
-    return view('admin/dashboard');
+Route::get('adminlogin', function () {
+    return view('admin/index');
 });
-Route::resource('car_details',CarDetailController::class);
-
-// Route::get('upload-car', function () {
-//     return view('uploadcar');
-// });
-
-// Route::get('car-listing', function () {
-//     return view('car-listing');
-// });
-
-
-// Route::post('post-to-server',[FileController::class, 'index']);
-
-
-
-Auth::routes();
- 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::get('upload-car',[CarDetailController::class,'create']);
-Route::get('car-listing',[CarDetailController::class,'index']);
-Route::get('cardetail/{car_details}',[CarDetailController::class,'show']);
-Route::get('myposts/{car_details}',[CarDetailController::class,'userpost']);
-Route::get('editcar/{CarDetail}',[CarDetailController::class,'edit']);
-Route::get('deletecar/{car_details}',[CarDetailController::class,'destroy']);
-Route::get('updatecar/{car_details}',[CarDetailController::class,'update']);
-Route::get('withdriver',[CarDetailController::class,'withdriver']);
-Route::get('withoutdriver',[CarDetailController::class,'withoutdriver']);
-Route::get('allcars',[CarDetailController::class,'allcars']);
-
-Route::get('reg-users',[RegisterController::class,'usersdata']);
-
-// Route::post('store-car-detail',[CarDetailController::class,'store']);
-
-
-
-
